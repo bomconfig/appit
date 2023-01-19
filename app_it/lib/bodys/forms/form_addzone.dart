8870fa056522b77aa658a87f751fdb7acac7d545
bomit.dev <75351905+bomconfig@users.noreply.github.com>
2023-01-19 11:39:35 +0700

    up

--- app_it/lib/bodys/forms/form_addzone.dart
+++ app_it/lib/bodys/forms/form_addzone.dart
@@ -1,8 +1,12 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:app_it/utility/myconstan.dart';
-import 'package:app_it/widget/show_image.dart';
+import 'package:app_it/utility/mydialog.dart';
+import 'package:dio/dio.dart';
+
 import 'package:flutter/material.dart';
+
 import 'package:image_picker/image_picker.dart';
 
 class FormAddZone extends StatefulWidget {
@@ -17,6 +21,8 @@ class _FormAddZoneState extends State<FormAddZone> {
   List<File?> files = [];
   File? file;
   final ImagePicker _picker = ImagePicker();
+
+  String? zone, detail, urlImage;
 
   @override
   void initState() {
@@ -41,22 +47,23 @@ class _FormAddZoneState extends State<FormAddZone> {
           builder: (context, constraints) => GestureDetector(
                 onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
                 behavior: HitTestBehavior.opaque,
-                child: SingleChildScrollView(
-                  child: Center(
-                    child: Column(
-                      children: [
-                        SizedBox(height: 30),
-                        addZone(size),
-                        SizedBox(height: 30),
-                        addDetailZone(size),
-                        SizedBox(height: 30),
-                        //buildImage(constraints),
-                        addImage(constraints),
-                        SizedBox(height: 30),
-                        buildButtonSave(constraints),
-                      ],
+                child: ListView(
+                  children: [
+                    Center(
+                      child: Column(
+                        children: [
+                          SizedBox(height: 30),
+                          addZone(size),
+                          SizedBox(height: 30),
+                          addDetailZone(size),
+                          SizedBox(height: 30),
+                          addImage(constraints),
+                          SizedBox(height: 30),
+                          saveZone(constraints),
+                        ],
+                      ),
                     ),
-                  ),
+                  ],
                 ),
               )),
     );
@@ -67,127 +74,46 @@ class _FormAddZoneState extends State<FormAddZone> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         IconButton(
-          onPressed: () {},
           icon: Icon(Icons.add_a_photo),
+          onPressed: () => processImagePiker(ImageSource.camera),
         ),
         Container(
-          width: 250,
-          child: Image.asset(MyConstant.gallery),
+          margin: const EdgeInsets.only(top: 15.0),
+          width: constraints.maxWidth * 0.50,
+          height: constraints.maxWidth * 0.50,
+          child: file == null
+              ? Image.asset(MyConstant.gallery)
+              : Image.file(file!),
         ),
         IconButton(
-          onPressed: () {},
           icon: Icon(Icons.add_photo_alternate),
+          onPressed: () => processImagePiker(ImageSource.gallery),
         ),
       ],
     );
   }
 
-  // Future<void> processImagePiker(ImageSource source, int index) async {
-  //   try {
-  //     var result = await _picker.pickImage(
-  //       source: source,
-  //       maxWidth: 400,
-  //       maxHeight: 400,
-  //     );
-  //     setState(() {
-  //       file = File(result!.path);
-  //       files[index] = file;
-  //     });
-  //   } catch (e) {
-  //     debugPrint('Failed to pick image: $e');
-  //   }
-  // }
-
-  // Future<void> chooseSourceImageDialog(int index) async {
-  //   debugPrint('Click from index ===> $index');
-  //   showDialog(
-  //     context: context,
-  //     builder: (context) => AlertDialog(
-  //       title: ListTile(
-  //         leading: ShowImagePage(path: MyConstant.logo1),
-  //         title: Text('source image ${index + 1} ?'),
-  //         subtitle: const Text('กรุณาเลือก Camera หรือ Gallery!'),
-  //       ),
-  //       actions: [
-  //         Row(
-  //           mainAxisAlignment: MainAxisAlignment.spaceAround,
-  //           children: [
-  //             TextButton(
-  //               onPressed: () {
-  //                 Navigator.pop(context);
-  //                 processImagePiker(ImageSource.camera, index);
-  //               },
-  //               child: const Text('Camera'),
-  //             ),
-  //             TextButton(
-  //               onPressed: () {
-  //                 Navigator.pop(context);
-  //                 processImagePiker(ImageSource.gallery, index);
-  //               },
-  //               child: const Text('Gallery'),
-  //             ),
-  //           ],
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
-
-  // Column buildImage(BoxConstraints constraints) {
-  //   return Column(
-  //     children: [
-  //       Container(
-  //           margin: const EdgeInsets.only(top: 15.0),
-  //           width: constraints.maxWidth * 0.50,
-  //           height: constraints.maxWidth * 0.50,
-  //           child: file == null
-  //               ? Image.asset(MyConstant.gallery)
-  //               : Image.file(file!)),
-  //       Container(
-  //         margin: const EdgeInsets.only(top: 5.0),
-  //         width: constraints.maxWidth * 0.8,
-  //         child: Row(
-  //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //           children: [
-  //             Container(
-  //               margin: const EdgeInsets.only(top: 5.0),
-  //               width: 48,
-  //               height: 48,
-  //               child: InkWell(
-  //                 onTap: () => chooseSourceImageDialog(0),
-  //                 child: files[0] == null
-  //                     ? Image.asset(MyConstant.gallery)
-  //                     : Image.file(
-  //                         files[0]!,
-  //                         fit: BoxFit.cover,
-  //                       ),
-  //               ),
-  //             ),
-  //             Container(
-  //               margin: const EdgeInsets.only(top: 5.0),
-  //               width: 48,
-  //               height: 48,
-  //               child: InkWell(
-  //                 onTap: () => chooseSourceImageDialog(1),
-  //                 child: files[1] == null
-  //                     ? Image.asset(MyConstant.gallery)
-  //                     : Image.file(
-  //                         files[1]!,
-  //                         fit: BoxFit.cover,
-  //                       ),
-  //               ),
-  //             ),
-  //           ],
-  //         ),
-  //       ),
-  //     ],
-  //   );
-  // }
+  Future<void> processImagePiker(ImageSource source) async {
+    try {
+      var result = await _picker.pickImage(
+        source: source,
+        maxWidth: 400,
+        maxHeight: 400,
+      );
+      setState(() {
+        file = File(result!.path);
+        // files[index] = file;
+      });
+    } catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
 
   Container addDetailZone(double size) {
     return Container(
       width: size * 0.75,
       child: TextFormField(
+        onChanged: ((value) => detail = value.trim()),
         decoration: InputDecoration(
           prefixIcon: Icon(
             Icons.location_on_outlined,
@@ -209,6 +135,7 @@ class _FormAddZoneState extends State<FormAddZone> {
     return Container(
       width: size * 0.75,
       child: TextFormField(
+        onChanged: ((value) => zone = value.trim()),
         decoration: InputDecoration(
           prefixIcon: Icon(
             Icons.location_on_outlined,
@@ -226,13 +153,46 @@ class _FormAddZoneState extends State<FormAddZone> {
     );
   }
 
-  Container buildButtonSave(BoxConstraints constraints) {
+  Future<Null> uploadImage() async {
+    Random random = Random();
+    int i = random.nextInt(100000);
+    String nameImageZone = 'zone$i.jpg';
+
+    try {
+      Map<String, dynamic> map = Map();
+      map['file'] =
+          await MultipartFile.fromFile(file!.path, filename: nameImageZone);
+
+      FormData formData = FormData.fromMap(map);
+      await Dio()
+          .post('${MyConstant.domain}/api_appit/saveAddFileZone.php',
+              data: formData)
+          .then((value) => {print('Response = $value')});
+      urlImage = '${MyConstant.domain}/api_appit/zone/$nameImageZone';
+      print('urlImage = $urlImage');
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Container saveZone(BoxConstraints constraints) {
     return Container(
       width: constraints.maxWidth * 0.25,
       margin: const EdgeInsets.only(top: 10.0, bottom: 30.0),
       child: ElevatedButton(
-        onPressed: () {},
-        child: Text('UP Load'),
+        onPressed: () {
+          if (zone == null ||
+              zone!.isEmpty ||
+              detail == null ||
+              detail!.isEmpty) {
+            MyDialog().normalDialog2(context, "กรุณากรอกให้ครบทุกช่อง");
+          } else if (file == null) {
+            MyDialog().normalDialog2(context, "กรุณาเลือก รูปภาพ");
+          } else {
+            uploadImage();
+          }
+        },
+        child: Text('Up Load'),
       ),
     );
   }
